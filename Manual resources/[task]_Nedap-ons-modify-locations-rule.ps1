c3c96c809aff07a1c55a5e01c8bf0461a77f44c1
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2021-10-11 16:56:05 +0200

    Beautified code

--- Manual resources/[task]_Nedap-ons-modify-locations-rule.ps1
+++ Manual resources/[task]_Nedap-ons-modify-locations-rule.ps1
@@ -2,28 +2,26 @@
 $Path = $NedapOnsLocationMappingPath
 
 $CSV = import-csv $Path -Delimiter ";"
-$filteredCSV = foreach($line in $CSV){
-    if(-not(($line.'Department.ExternalId' -eq $organisationalUnit) -and ($line.NedapLocationIds -eq $locationsOriginal))){
+$filteredCSV = foreach ($line in $CSV) {
+    if (-not(($line.'Department.ExternalId' -eq $organisationalUnit) -and ($line.NedapLocationIds -eq $locationsOriginal))) {
         $line 
     }
 }
-$filteredCSV | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | % {$_.Replace('"','')} | Out-File $Path
+$filteredCSV | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | % { $_.Replace('"', '') } | Out-File $Path
 
 #Step 2 - add new rule definition
 $afasLocation = $organisationalUnit
 $nedapLocations = $locationsNew | ConvertFrom-Json
 
-foreach($n in $nedapLocations)
-{
+foreach ($n in $nedapLocations) {
     $nedapLocationString = $nedapLocationString + $n.Id.ToString() + ","
 }
 
-$nedapLocationString = $nedapLocationString.Substring(0,$nedapLocationString.Length-1)
+$nedapLocationString = $nedapLocationString.Substring(0, $nedapLocationString.Length - 1)
 
 $rule = [PSCustomObject]@{
     "Department.ExternalId" = $afasLocation;
-    "NedapLocationIds"= $nedapLocationString;
+    "NedapLocationIds"      = $nedapLocationString;
 }
 
-$rule | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | % { $_ -replace '"', ""}  | Select-Object -Skip 1  | Add-Content $Path -Encoding UTF8
-
+$rule | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | ForEach-Object { $_ -replace '"', "" }  | Select-Object -Skip 1  | Add-Content $Path -Encoding UTF8
